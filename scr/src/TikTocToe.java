157502dbdc22faa2e1d13073d28f726ca70d4dee
Mokey-D-luffy88 <atching123@gmail.com>
2020-04-11 14:04:30 +0530

    this funtion will continue call user untill one of them wins the game.

--- scr/src/TikTocToe.java
+++ scr/src/TikTocToe.java
@@ -3,6 +3,18 @@ public class TikTocToe {
     private static char[][] ticTocToe = {{' ', ' ', ' '},
             {' ', ' ', ' '},
             {' ', ' ', ' '}};
+    private static boolean comeOut = false;
 
+    public static void main(String[] args) {
+        int threeComplete = 0;
+        while (!comeOut) {
+            userInput();
+            if (threeComplete >= ticTocToe.length) {
+                checkResult();
+            }
+
+        }
+
+    }
 
 }
