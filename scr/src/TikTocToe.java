a56651f746f8ad977cc198760d014bbd6a2f1be0
Mokey-D-luffy88 <atching123@gmail.com>
2020-04-11 14:21:17 +0530

    this asked the location to put x or o untill they give an right location

--- scr/src/TikTocToe.java
+++ scr/src/TikTocToe.java
@@ -1,3 +1,5 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class TikTocToe {
     private static char[][] ticTocToe = {{' ', ' ', ' '},
@@ -16,5 +18,27 @@ public class TikTocToe {
         }
 
     }
+
+    private static void userInput() {
+        int row = 0;
+        int column = 0;
+        Scanner tell = new Scanner(System.in);
+        boolean comeOut = false;
+        while (!comeOut) {
+            System.out.println("Enter the coordinates: ");
+            try {
+                row = tell.nextInt();
+                column = tell.nextInt();
+                comeOut = true;
+            } catch (InputMismatchException e) {
+                System.out.println("You should enter numbers!");
+                System.out.print("Enter the coordinates: ");
+                tell.nextLine();
+            }
+        }
+
+
+    }
+}
 
 }
